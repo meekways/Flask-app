50442b54a5f19fb8228052de07f55c7bb2136436
Williams Yakubu <meekways123@gmail.com>
2022-02-27 19:18:21 +0100

    installed sqlalchemy and initiated db model

--- app.py
+++ app.py
@@ -1,18 +1,39 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+import os
+
 
 app = Flask(__name__)
 
-todoData = []
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    todo = db.Column(db.String(100), unique=True, nullable=False, primary_key= True)
+
+
+#finding the current app path. (Location of this file)
+project_dir = os.path.dirname(os.path.abspath(__file__))
+
+#creating the database file (bookdatabase.db) to the sqlalchemy dependency
+database_file = "sqlite:///{}".format(os.path.join(project_dir, "todo.db"))
+
+app.config["SQLALCHEMY_DATABASE_URI"]= database_file
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 
 @app.route('/')
 def index():
     return render_template("index.html", todos = todoData)
 
+todoData = [] #stores data entered
+update = []
+
+
+
 @app.route('/create-todo', methods=["POST"])
 def create_todo():
-    new_todo = request.form.get("new_todo")
-    todoData.append(new_todo)
-    print(todoData)
+    new_todo = request.form.get("new_todo") #create a variable that stores form data
+    todoData.append(new_todo) #appends data to an array
+    print(todoData) #prints array of things to do
     return redirect(url_for("index"))
 
 @app.route("/delete-todo/<todo_item>")
